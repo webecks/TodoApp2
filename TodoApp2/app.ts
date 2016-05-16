01ca5f83bc61155be976957902aa104f87660f4e
Donny Liem <donnyprabowo@hotmail.com>
2016-05-16 16:36:31 +0700

    version 1

--- TodoApp2/app.ts
+++ TodoApp2/app.ts
@@ -26,3 +26,47 @@ window.onload = () => {
     var greeter = new Greeter(el);
     greeter.start();
 };
+
+import {Component, View, bootstrap, NgFor} from 'angular2/angular2';
+
+@Component({
+    selector: "my-app",
+
+})
+
+@View({
+    templateUrl: 'mainForm.html',
+    directives: [NgFor]
+})
+
+class TodoAppComponent {
+
+    todos: Array<Object>;
+
+    constructor() {
+        this.todos = [{ text: "Try Second", done: true }]
+    }
+
+    get() {
+        return this.todos;
+    }
+
+    add($event, newtodo) {
+
+        if ($event.which === 13) {
+            this.todos.unshift({ text: newtodo.value, done: false });
+            newtodo.value = ''
+        }
+        if ($event.which === 46) {
+            this.todos.pop();
+           
+        }
+
+    }
+
+    markAsDone(todo) {
+        todo.done = !todo.done;
+    }
+}
+
+bootstrap(TodoAppComponent);
